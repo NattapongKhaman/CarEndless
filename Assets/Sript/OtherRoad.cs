22070c94701a1b1740d20b52dd307f6645da6ec0
NattapongKhaman <Nattapong.khama@bumail.net>
2025-05-10 23:23:16 +0700

    V1

--- Assets/Sript/OtherRoad.cs
+++ Assets/Sript/OtherRoad.cs
@@ -3,30 +3,30 @@ using UnityEngine;
 
 public class OtherRoad : MonoBehaviour
 { 
-    public GameObject road;
     public GameObject otherRoad;
     public float dawnMax = 30 ;
-    
     private Vector3 startPos;
-   
     private float speed;
-    
-
+    private Road roadMain;
     void Start()
     {
-        
+        //รับค้าจากRoad ที่อยู่ใน Roadmain
+        roadMain = GameObject.Find("RoadMain").GetComponent<Road>();
         startPos = otherRoad.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        speed = road.GetComponent<Road>().speed;
+        //รับค่าความเร็วจากRoadMain
+        speed = roadMain.GetComponent<Road>().speed;
         if (transform.position.z < -dawnMax)
         {
             transform.position = startPos;
         }
         transform.Translate(Vector3.forward * Time.deltaTime * -speed);
+        
+
 
     }
 }
--- Assets/Sript/Road.cs
+++ Assets/Sript/Road.cs
@@ -4,20 +4,38 @@ public class Road : MonoBehaviour
 {
     private Vector3 startPos;
     public float speed = 0.1f;
+    private PlayerCon playerCon;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //รับค่า จุดรัถนน กับ PlayerCon
         startPos = transform.position;
+        playerCon = GameObject.Find("Player").GetComponent<PlayerCon>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed *=2;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed /=2;
+        }
+        // หยุดเมื่อตาย
+        if (playerCon.hp == 0)
+        {
+            speed = 0;
+        }
+        //วนถนน
         if (transform.position.z < -20)
         {
             transform.position = startPos;
         }
+        //ขยับถนน
         transform.Translate(Vector3.forward * Time.deltaTime * -speed);
     }
 }
